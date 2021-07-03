894cc398f0db4ff44e8d1c0d2d7b5296b0e63df7
Filipe Soares <filipeseventeen1@gmail.com>
2021-07-02 22:29:39 -0300

    Foi trocada a url e foi adicionada também ao next-config

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -6,7 +6,7 @@ export function getAPIClient(ctx?: any) {
   const { 'mydiary-token': token } = parseCookies(ctx)
 
   const api = axios.create({
-    baseURL: 'http://localhost:3333/'
+    baseURL: 'https://flpmydiary.herokuapp.com/'
   })
 
   if (token) {
